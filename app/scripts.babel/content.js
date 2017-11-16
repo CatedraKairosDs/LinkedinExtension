fde5dc45de563c04a4c259f082e070246080fbdd
diemacre <18407921+diemacre@users.noreply.github.com>
2017-11-16 12:50:44 +0100

    intentoAccesoDomPruebaFallido2

--- app/scripts.babel/content.js
+++ app/scripts.babel/content.js
@@ -1,3 +1,7 @@
+//no hagas mucho caso, he hecho varias pruebas pero no consigo 
+//que funcione, lo que es curioso es que ahora el confirm
+//se mantiene jaja
+
 /* Listen for messages */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     /* If the received message has the expected format... */
@@ -7,3 +11,7 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     sendResponse(document.getElementById("mybutton").innerHTML);
     }
 });
+confirm("Hello from your Chrome extension!");
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+console.log(firstHref);
