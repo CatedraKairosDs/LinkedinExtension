3962c218f511a673995a0b8ea89dbe1fc768d372
diemacre <18407921+diemacre@users.noreply.github.com>
2017-11-16 09:28:20 +0100

    confirmacion alfa

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -6,28 +6,44 @@ chrome.runtime.onInstalled.addListener(details => {
 
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+//console.log('\'Allo \'Allo! Event Page for Browser Action');
 
 var selectorPuesto = document.getElementById("SelectorPuesto");
 var acceptButton = document.getElementById("accept");
 var maybeButton = document.getElementById("maybe");
 var refuseButton = document.getElementById("refuse");
-
 var puesto = selectorPuesto.options[selectorPuesto.selectedIndex].text;
+var confirmacion = document.getElementById("confirmacion");
+var resultado = document.getElementById("resultado");
+confirmacion.style.display = 'none';
+resultado.style.display = 'none';
+var acept=  document.getElementById("acept");
+var cancel=  document.getElementById("cancel");
 
 selectorPuesto.addEventListener('change', function(event){
   console.log("Ha cambiado");
   puesto = selectorPuesto.options[selectorPuesto.selectedIndex].text;
   console.log("Dentro: " + puesto)
+  confirmacion.style.display = 'none';
+  resultado.style.display = 'none';
 });
 
-acceptButton.addEventListener('click', function(event){
-  confirm("Estás aceptando un perfil de "+puesto);
-  console.log("Acceptado para el puesto: "+puesto);
+acceptButton.addEventListener("click", function(event){
+  var txt;
+  var r = confirm("Estás aceptando un perfil de "+puesto);
+  console.log(r);
+  if (r == true) {
+    txt = "Acceptado para el puesto: "+puesto;
+  } else {
+    txt = "Cancelado!";
+  }
+  console.log(txt);
+  document.getElementById("confirmacion").innerHTML = txt;
 });
 
 maybeButton.addEventListener('click', function(event){
-  confirm('Estás "metiendo en la nevera" un perfil de '+puesto);
+  confirmacion.style.display = '';
+  //confirm('Estás "metiendo en la nevera" un perfil de '+puesto);
   console.log("Quizás para el puesto: "+puesto);
 });
 
@@ -36,6 +52,18 @@ refuseButton.addEventListener('click', function(event){
   console.log("Rechazado para el puesto: "+puesto);
 });
 
+acept.addEventListener('click', function(event){
+  confirmacion.style.display = 'none';
+  document.getElementById("resultado").innerHTML= "Acceptado para el puesto: "+puesto;
+  resultado.style.display = '';
+  
+});
+cancel.addEventListener('click', function(event){
+  confirmacion.style.display = 'none';
+  document.getElementById("resultado").innerHTML= "Cancelado"
+  resultado.style.display = '';
+  
+});
 console.log("Fuera: "+puesto);
 
 
