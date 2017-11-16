5bfaf796ba346436414f7e420610040e6dd87fcc
Jaime <Jaime@Jaimes-MacBook-Air.local>
2017-11-16 09:41:10 +0100

    Formularios funcionando

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -8,35 +8,7 @@ chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
 
-var selectorPuesto = document.getElementById("SelectorPuesto");
-var acceptButton = document.getElementById("accept");
-var maybeButton = document.getElementById("maybe");
-var refuseButton = document.getElementById("refuse");
-
-var puesto = selectorPuesto.options[selectorPuesto.selectedIndex].text;
-
-selectorPuesto.addEventListener('change', function(event){
-  console.log("Ha cambiado");
-  puesto = selectorPuesto.options[selectorPuesto.selectedIndex].text;
-  console.log("Dentro: " + puesto)
-});
-
-acceptButton.addEventListener('click', function(event){
-  confirm("Estás aceptando un perfil de "+puesto);
-  console.log("Acceptado para el puesto: "+puesto);
-});
-
-maybeButton.addEventListener('click', function(event){
-  confirm('Estás "metiendo en la nevera" un perfil de '+puesto);
-  console.log("Quizás para el puesto: "+puesto);
-});
-
-refuseButton.addEventListener('click', function(event){
-  prompt("Estás rechazando un perfil para el puesto de "+puesto+"\n ¿Por qué?");
-  console.log("Rechazado para el puesto: "+puesto);
-});
-
-console.log("Fuera: "+puesto);
-
-
+// Regex-pattern to check URLs against. 
+// It matches URLs like: http[s]://[...]linkedin.com[...]
+var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?linkedin\.com/;
 
