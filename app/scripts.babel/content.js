fc4d20b5a826e28b11278e059cebbc9bb178eb2d
Jaime <Jaime@Jaimes-MacBook-Pro.local>
2017-12-20 17:09:01 +0100

    Solucionado innerText

--- app/scripts.babel/content.js
+++ app/scripts.babel/content.js
@@ -17,10 +17,10 @@ chrome.runtime.onMessage.addListener(
             return;
         }
         //General
-        var name = document.getElementsByClassName('profile-info')[0].childNodes[0].innerHTML;
+        var name = document.getElementsByClassName('profile-info')[0].childNodes[0].innerText;
         var extract = '';
         if (document.getElementById('profile-summary')) {
-            extract = document.getElementById('profile-summary').childNodes[1].innerHTML;
+            extract = document.getElementById('profile-summary').childNodes[1].innerText;
         }
         console.log('Nombre: ', name);
         //Experiencia
@@ -29,32 +29,32 @@ chrome.runtime.onMessage.addListener(
             var experienceAll = document.getElementById('profile-experience').childNodes[1].childNodes[0].childNodes;
             for (var i = 0; i < experienceAll.length; i++) {
                 var childAux = experienceAll[i].childNodes[0];
-                var puestoName = childAux.childNodes[0].childNodes[0].innerHTML;
-                var cmpny = childAux.childNodes[1].childNodes[0].innerHTML;
+                var puestoName = childAux.childNodes[0].childNodes[0].innerText;
+                var cmpny = childAux.childNodes[1].childNodes[0].innerText;
                 var cuando = childAux.childNodes[2].childNodes[0].nodeValue;
                 var description = '';
                 var cuanto = '';
                 var donde = '';
                 if ((experienceAll[i].childNodes[1]) && (experienceAll[i].childNodes.length > 2)){
                     if (experienceAll[i].childNodes[1].target != '_blank') {
-                        description = experienceAll[i].childNodes[1].innerHTML;
+                        description = experienceAll[i].childNodes[1].innerText;
                     } else {
-                        description = experienceAll[i].childNodes[2].innerHTML;
+                        description = experienceAll[i].childNodes[2].innerText;
                     }
                 } else {
                     description = '';
                 }
                 if (childAux.childNodes[2].childNodes[2]) {
-                    donde = childAux.childNodes[2].childNodes[2].innerHTML;
+                    donde = childAux.childNodes[2].childNodes[2].innerText;
                     if (childAux.childNodes[2].childNodes[1]){
-                        cuanto = childAux.childNodes[2].childNodes[1].innerHTML;
+                        cuanto = childAux.childNodes[2].childNodes[1].innerText;
                     } else {
                         cuanto = '';
                     }
                 } else {
                     donde = '';
                     if (childAux.childNodes[2].childNodes[1]){
-                        cuanto = childAux.childNodes[2].childNodes[1].innerHTML;
+                        cuanto = childAux.childNodes[2].childNodes[1].innerText;
                     } else {
                         cuanto = '';
                     }
@@ -79,10 +79,10 @@ chrome.runtime.onMessage.addListener(
             var idiomaAll = document.getElementById('profile-language').childNodes[1].childNodes[0].childNodes;
             for (var i = 0; i < idiomaAll.length; i++) {
                 var idioma = idiomaAll[i];
-                var lang = idioma.childNodes[0].innerHTML;
+                var lang = idioma.childNodes[0].innerText;
                 var level = '';
                 if (idioma.childNodes[1]) {
-                    level = idioma.childNodes[1].innerHTML;
+                    level = idioma.childNodes[1].innerText;
                 }
                 var jsonIdioma = {
                     lang: lang,
@@ -100,9 +100,9 @@ chrome.runtime.onMessage.addListener(
             var educationAll = document.getElementById('profile-education').childNodes[1].childNodes[0].childNodes;
             for (var i = 0; i < educationAll.length; i++) {
                 var education = educationAll[i].childNodes[0];
-                var uni = education.childNodes[0].childNodes[0].innerHTML;
-                var eduName = education.childNodes[1].innerHTML;
-                var eduWhen = education.childNodes[2].innerHTML;
+                var uni = education.childNodes[0].childNodes[0].innerText;
+                var eduName = education.childNodes[1].innerText;
+                var eduWhen = education.childNodes[2].innerText;
                 var jsonEducation = {
                     university: uni,
                     eduName: eduName,
@@ -119,10 +119,10 @@ chrome.runtime.onMessage.addListener(
             var projects = document.getElementById('profile-projects').childNodes[1].childNodes[0].childNodes;
             for (var i = 0; i < projects.length; i++) {
                 var project = projects[i];
-                var projectName = project.childNodes[0].childNodes[0].innerHTML;
-                var projectTime = project.childNodes[0].childNodes[1].innerHTML;
+                var projectName = project.childNodes[0].childNodes[0].innerText;
+                var projectTime = project.childNodes[0].childNodes[1].innerText;
                 if (project.childNodes[1].className == 'description searchable') {
-                    var description = project.childNodes[1].innerHTML;
+                    var description = project.childNodes[1].innerText;
                     var jsonProject = {
                         projectName: projectName,
                         projectTime: projectTime,
@@ -145,7 +145,7 @@ chrome.runtime.onMessage.addListener(
         var skillsAll = document.getElementById('profile-skills').childNodes[1].childNodes[0].childNodes;
         var jsonSkills = [];
         for (var i = 0; i < skillsAll.length; i++) {
-            var skill = skillsAll[i].innerHTML;
+            var skill = skillsAll[i].innerText;
             var jsonSkill = {
                 skill: skill
             };
@@ -158,9 +158,9 @@ chrome.runtime.onMessage.addListener(
             var certificates = document.getElementById('profile-certifications').childNodes[1].childNodes[0].childNodes;
             for (var i = 0; i < certificates.length; i++) {
                 var certificado = certificates[i];
-                var certName = certificado.childNodes[0].childNodes[0].innerHTML;
-                var licenciaCert = certificado.childNodes[0].childNodes[1].innerHTML;
-                var whenCert = certificado.childNodes[0].childNodes[2].innerHTML;
+                var certName = certificado.childNodes[0].childNodes[0].innerText;
+                var licenciaCert = certificado.childNodes[0].childNodes[1].innerText;
+                var whenCert = certificado.childNodes[0].childNodes[2].innerText;
                 var jsonCertificate = {
                     certName: certName,
                     certLicense: licenciaCert,
@@ -179,10 +179,10 @@ chrome.runtime.onMessage.addListener(
             var awards = document.getElementById('profile-honors').childNodes[1].childNodes[0].childNodes;
             for (var i = 0; i < awards.length; i++) {
                 var award = awards[i];
-                var position = award.childNodes[0].childNodes[0].innerHTML;
-                var organisation = award.childNodes[0].childNodes[1].innerHTML;
-                var awardTime = award.childNodes[0].childNodes[2].innerHTML;
-                var awardDescription = award.childNodes[1].innerHTML;
+                var position = award.childNodes[0].childNodes[0].innerText;
+                var organisation = award.childNodes[0].childNodes[1].innerText;
+                var awardTime = award.childNodes[0].childNodes[2].innerText;
+                var awardDescription = award.childNodes[1].innerText;
                 var jsonAward = {
                     position: position,
                     orginisation: organisation,
@@ -202,9 +202,9 @@ chrome.runtime.onMessage.addListener(
             var organizaciones = document.getElementById('profile-orgs').childNodes[1].childNodes[0].childNodes;
             for (var i = 0; i < organizaciones.length; i++) {
                 var organizacion = organizaciones[i];
-                var orgName = organizacion.childNodes[0].childNodes[0].innerHTML;
-                var positionInOrg = organizacion.childNodes[0].childNodes[1].innerHTML;
-                var timeInOrg = organizacion.childNodes[0].childNodes[2].innerHTML;
+                var orgName = organizacion.childNodes[0].childNodes[0].innerText;
+                var positionInOrg = organizacion.childNodes[0].childNodes[1].innerText;
+                var timeInOrg = organizacion.childNodes[0].childNodes[2].innerText;
                 var jsonOrg = {
                     orgName: orgName,
                     posInOrg: positionInOrg,
@@ -224,10 +224,10 @@ chrome.runtime.onMessage.addListener(
             var volunteerExperiences = document.getElementById('profile-volunteer').childNodes[1].childNodes[0].childNodes;
             for (var i = 0; i < volunteerExperiences.length; i++) {
                 var volunteerExperience = volunteerExperiences[i];
-                var nameExp = volunteerExperience.childNodes[0].childNodes[0].innerHTML;
-                var orgExp = volunteerExperience.childNodes[0].childNodes[1].innerHTML;
+                var nameExp = volunteerExperience.childNodes[0].childNodes[0].innerText;
+                var orgExp = volunteerExperience.childNodes[0].childNodes[1].innerText;
                 var whenExp = volunteerExperience.childNodes[0].childNodes[2].childNodes[0].nodeValue;
-                var causeExp = volunteerExperience.childNodes[0].childNodes[2].childNodes[1].innerHTML;
+                var causeExp = volunteerExperience.childNodes[0].childNodes[2].childNodes[1].innerText;
                 var descrExp = ''
                 if (volunteerExperience.childNodes[1].childNodes[0]) {
                     descrExp = volunteerExperience.childNodes[1].childNodes[0].nodeValue;
@@ -248,7 +248,7 @@ chrome.runtime.onMessage.addListener(
             if (document.getElementById('profile-volunteer').childNodes[1].childNodes[1]) {
                 var beneficCauses = document.getElementById('profile-volunteer').childNodes[1].childNodes[1].childNodes[1].childNodes;
                 for (var i = 0; i < beneficCauses.length; i++) {
-                    var beneficCause = beneficCauses[i].innerHTML;
+                    var beneficCause = beneficCauses[i].innerText;
                     var jsonBenefCause = {
                         beneficCause: beneficCause
                     };
@@ -268,10 +268,10 @@ chrome.runtime.onMessage.addListener(
             var recommendations = document.getElementById('profile-recommendations').childNodes[1].childNodes[0].childNodes;
             for (var i = 0; i < recommendations.length; i++) {
                 var recommendation = recommendations[i];
-                var whoRec = recommendation.childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[0].innerHTML;
-                var posWhoRec = recommendation.childNodes[1].childNodes[0].childNodes[1].childNodes[1].innerHTML;
-                var whereWhoRec = recommendation.childNodes[1].childNodes[0].childNodes[1].childNodes[2].innerHTML;
-                var descrRec = recommendation.childNodes[1].childNodes[0].childNodes[1].childNodes[3].innerHTML;
+                var whoRec = recommendation.childNodes[1].childNodes[0].childNodes[1].childNodes[0].childNodes[0].innerText;
+                var posWhoRec = recommendation.childNodes[1].childNodes[0].childNodes[1].childNodes[1].innerText;
+                var whereWhoRec = recommendation.childNodes[1].childNodes[0].childNodes[1].childNodes[2].innerText;
+                var descrRec = recommendation.childNodes[1].childNodes[0].childNodes[1].childNodes[3].innerText;
                 var jsonRec = {
                     whoRecommended: whoRec,
                     positionOfRecomender: posWhoRec,
